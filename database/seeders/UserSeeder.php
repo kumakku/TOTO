610e58b95f7f56ab885f3e199bc525c8a8e14814
Jamese-P <syuntaokuhara0812@gmail.com>
2023-08-26 15:23:11 +0900

    modify breeze

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use DateTimeImmutable;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+final class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'created_at' => new DateTimeImmutable(),
+            'updated_at' => new DateTimeImmutable(),
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
 /*
