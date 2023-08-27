a31a333b600d9b6f63054ae9712796551e51bb30
Jamese-P <syuntaokuhara0812@gmail.com>
2023-08-27 14:07:50 +0900

    feat category

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -7,16 +7,31 @@
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
-        <h1>チーム開発会へようこそ！</h1>
         <h2>カテゴリー:{{ $category_name }} の投稿一覧画面</h2>
         <a href='/'>投稿一覧ページへ戻る</a>
         <div>
             @foreach ($posts as $post)
-                <div style='border:solid 1px; margin-bottom: 10px;'>
+                <div class="post" style='border:solid 1px; margin-bottom: 10px;'>
+                    <p>
+                        ユーザー名：<a href="/users/{{$post->user_id}}">{{ $post->user->name }}</a>
+                    </p>
                     <p>
                         タイトル：<a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
                     </p>
+                    @if($post->image_url)
+                    <div>
+                        <img src="{{ $post->image_url }}" alt="画像が読み込めません。"/>
+                    </div>
+                    @endif
                     <p>カテゴリー：<a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a></p>
+                    <p>♡　{{ $post->likes->count() }}</p>
+                    <div>
+                        @if($post->is_liked_by_auth_user())
+                            <a href="/unlike/{{ $post->id }}" class="btn btn-success btn-sm">いいね  <span class="badge">{{ $post->likes->count() }}</span></a>
+                        @else
+                            <a href="/like/{{ $post->id }}" class="btn btn-secondary btn-sm">いいね  <span class="badge">{{ $post->likes->count() }}</span></a>
+                        @endif
+                    </div>
                 </div>
             @endforeach
         </div>
